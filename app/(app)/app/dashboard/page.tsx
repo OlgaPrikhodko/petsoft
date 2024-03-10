a1591bde492a56530f1cdef404b8e86c030eec41
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-10 13:09:52 +0200

    🌟 Dashboard: build content grid layout

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from "@/components/Branding";
+import ContentBlock from "@/components/ContentBlock";
+import PetDetails from "@/components/PetDetails";
+import PetList from "@/components/PetList";
+import SearchForm from "@/components/SearchForm";
 import Stats from "@/components/Stats";
 
 const DashboardPage = () => {
@@ -7,6 +11,24 @@ const DashboardPage = () => {
       <div className="flex items-center justify-between py-8 text-white">
         <Branding />
         <Stats />
+      </div>
+
+      <div className="grid h-[600px] grid-cols-3 grid-rows-[45px_1fr] gap-4">
+        <div className="col-span-1 col-start-1 row-span-1 row-start-1">
+          <SearchForm />
+        </div>
+
+        <div className="col-span-1 col-start-1 row-span-full row-start-2">
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className="col-span-full col-start-2 row-span-full row-start-1">
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
