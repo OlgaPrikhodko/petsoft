cf7655933f8fe9e0793b94296911dab5e887df0d
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-11 07:24:04 +0200

    :star: fetch and display pet list

--- components/PetList.tsx
+++ components/PetList.tsx
@@ -1,9 +1,23 @@
-export default function PetList() {
+import { Pet } from "@/lib/types";
+import Image from "next/image";
+
+export default function PetList({ pets }: { pets: Pet[] }) {
   return (
     <ul className="border-b border-black/[0.08] bg-white">
-      <li>
-        <button>Benjamin </button>
-      </li>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex h-[70px] w-full cursor-pointer items-center gap-3 px-5 text-base transition hover:bg-[#eff1f2] focus:bg-[#eff1f2]">
+            <Image
+              className="h-[45px] w-[45px] rounded-full object-cover"
+              width={45}
+              height={45}
+              alt="Pet image"
+              src={pet.imageUrl}
+            />
+            <p className="font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
