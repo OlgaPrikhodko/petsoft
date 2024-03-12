37c4c2333ba0a9b813710d1ae0048a01275b7e47
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-12 08:57:03 +0200

    🌟 build Account Page

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,4 +1,14 @@
-const AccountPage = () => {
-  return <div>AccountPage</div>;
-};
-export default AccountPage;
+import ContentBlock from "@/components/ContentBlock";
+import H1 from "@/components/H1";
+
+export default function AccountPage() {
+  return (
+    <main>
+      <H1 className="my-8 text-white">Your Account</H1>
+
+      <ContentBlock className="flex h-[500px] items-center justify-center">
+        <p>Logged in as ...</p>
+      </ContentBlock>
+    </main>
+  );
+}
