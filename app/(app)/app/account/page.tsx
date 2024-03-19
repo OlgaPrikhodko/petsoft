cbcfa54e459d504cc95147f8f6bcae5c1fd2d127
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-19 15:18:37 +0200

    :star: Auth: sign out with server action

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,13 +1,22 @@
 import ContentBlock from "@/components/ContentBlock";
 import H1 from "@/components/H1";
+import SignOutBtn from "@/components/SignOutBtn";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="flex h-[500px] items-center justify-center">
-        <p>Logged in as ...</p>
+      <ContentBlock className="flex h-[500px] flex-col items-center justify-center gap-y-3">
+        <p>Logged in as {session.user.email}</p>
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
