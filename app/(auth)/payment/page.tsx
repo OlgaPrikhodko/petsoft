a7d421f112a356c805d041fa1a37b2cda193e683
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-22 15:32:54 +0200

    :star: Payment: success and cancelled message after payment depend on page's searchParams

--- app/(auth)/payment/page.tsx
+++ app/(auth)/payment/page.tsx
@@ -4,14 +4,33 @@ import { createCheckoutSession } from "@/actions/payment-actions";
 import H1 from "@/components/H1";
 import { Button } from "@/components/ui/button";
 
-export default function PaymentPage() {
+type PageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default function PaymentPage({ searchParams }: PageProps) {
   return (
     <main className="space-y-10 text-center">
       <H1>PetSoft access requires payment</H1>
 
-      <Button onClick={async () => await createCheckoutSession()}>
-        Buy lifetime access for $299
-      </Button>
+      {!searchParams.success && (
+        <Button onClick={async () => await createCheckoutSession()}>
+          Buy lifetime access for $299
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <p className="taxt-sm text-green-700">
+          Congratulation! Payment is successful! Now you have lifetime access to
+          PetSoft.
+        </p>
+      )}
+
+      {searchParams.cancelled && (
+        <p className="text-sm text-red-700">
+          Payment cancelled. You can try again.
+        </p>
+      )}
     </main>
   );
 }
