4206d5b9daab18534167e68f89184f4a71b5c307
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-22 15:43:59 +0200

    :star: Payment: checkout session loading state

--- app/(auth)/payment/page.tsx
+++ app/(auth)/payment/page.tsx
@@ -3,18 +3,26 @@
 import { createCheckoutSession } from "@/actions/payment-actions";
 import H1 from "@/components/H1";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 type PageProps = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export default function PaymentPage({ searchParams }: PageProps) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="space-y-10 text-center">
       <H1>PetSoft access requires payment</H1>
 
       {!searchParams.success && (
-        <Button onClick={async () => await createCheckoutSession()}>
+        <Button
+          disabled={isPending}
+          onClick={async () => {
+            startTransition(async () => await createCheckoutSession());
+          }}
+        >
           Buy lifetime access for $299
         </Button>
       )}
