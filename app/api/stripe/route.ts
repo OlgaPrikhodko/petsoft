73d9ce287c98a3489812a86e8e72e1159e673df5
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-03-25 11:30:40 +0200

    :star: Payment: receive webhooks locally through Ngrok and fullfill order (database update)

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,16 @@
+import prisma from "@/lib/db";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  // verify webhook came from Stripe
+
+  // fullfill order
+  await prisma.user.update({
+    where: { email: data.data.object.customer_email },
+    data: { hasAccess: true },
+  });
+
+  // return 200 OK
+  return Response.json(null, { status: 200 });
+}
